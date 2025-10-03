6e50595679cb9cd5ee756290a7cec03277675a08
Serebryakov <createrdany@gmail.com>
2025-10-03 16:24:10 +0300

    eleven_com

--- main.go
+++ main.go
@@ -72,36 +72,36 @@ func fetchStats() ([]float64, error) {
 }
 
 func checkMetrics(stats []float64) {
-	// 0 - Load Average
-	if stats[0] > 30 {
-		fmt.Printf("Load Average is too high: %.0f\n", stats[0])
-	}
-	
-	// 1 - Total RAM, 2 - Used RAM (>80%)
-	if stats[1] > 0 {
-		memoryUsagePercent := (stats[2] / stats[1]) * 100
-		if memoryUsagePercent > 80 {
-			fmt.Printf("Memory usage too high: %.0f%%\n", memoryUsagePercent)
-		}
-	}
-	
-	// 3 - Total Disk, 4 - Used Disk (<10% free)
-	if stats[3] > 0 {
-		freeDiskBytes := stats[3] - stats[4]
-		freeDiskPercent := (freeDiskBytes / stats[3]) * 100
-		if freeDiskPercent < 10 {
-			freeDiskMB := freeDiskBytes / (1024 * 1024)
-			fmt.Printf("Free disk space is too low: %.0f Mb left\n", freeDiskMB)
-		}
-	}
-	
-	// 5 - Total Network, 6 - Used Network (>90%)
-	if stats[5] > 0 {
-    networkUsagePercent := (stats[6] / stats[5]) * 100
-    if networkUsagePercent > 90 {
-        availableBandwidthBytes := stats[5] - stats[6]
-        availableBandwidthMbps := availableBandwidthBytes / (1024 * 125) // 1 Мбит/с = 125000 байт/с
-        fmt.Printf("Network bandwidth usage high: %.0f Mbit/s available\n", availableBandwidthMbps)
+    // 0 - Load Average
+    if stats[0] > 30 {
+        fmt.Printf("Load Average is too high: %.0f\n", stats[0])
     }
-}
+    
+    // 1 - Total RAM, 2 - Used RAM (>80%)
+    if stats[1] > 0 {
+        memoryUsagePercent := int((stats[2] / stats[1]) * 100)
+        if memoryUsagePercent > 80 {
+            fmt.Printf("Memory usage too high: %d%%\n", memoryUsagePercent)
+        }
+    }
+    
+    // 3 - Total Disk, 4 - Used Disk (<10% free)
+    if stats[3] > 0 {
+        freeDiskBytes := stats[3] - stats[4]
+        freeDiskPercent := (freeDiskBytes / stats[3]) * 100
+        if freeDiskPercent < 10 {
+            freeDiskMB := freeDiskBytes / (1024 * 1024)
+            fmt.Printf("Free disk space is too low: %.0f Mb left\n", freeDiskMB)
+        }
+    }
+    
+    // 5 - Total Network, 6 - Used Network (>90%)
+    if stats[5] > 0 {
+        networkUsagePercent := (stats[6] / stats[5]) * 100
+        if networkUsagePercent > 90 {
+            availableBandwidthBytes := stats[5] - stats[6]
+            availableBandwidthMbps := availableBandwidthBytes * 8 / (1024 * 1024) // 1 Мбит/с = 125000 байт/с
+            fmt.Printf("Network bandwidth usage high: %.0f Mbit/s available\n", availableBandwidthMbps)
+        }
+    }
 }
